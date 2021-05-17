f85d5daec7048f7c49f6603e726b89fb7a4f8ba2
marcio1989 <marcio.ricardo.muniz@hotmail.com>
2021-05-16 21:50:44 -0300

    Iniciando projeto com laravel

---
+++ resources/views/layout/Usuario.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Htpp\Controllers;
+
+use Illuminate\Http\Request;
+
+class Usuario extends Controller
+{
+    public function cadastrar(){
+        return view('layout.base');
+    }
+    public function salvar(Request $request){
+        dd($request->all());
+    }
+}
